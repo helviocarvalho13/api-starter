bde2b272cb320f3e84ca8227d1720155a4e3409c
helvio.carvalho <helvio.carvalho@pdcase.com.br>
2020-08-23 13:42:54 -0300

    Commit.

--- src/main/java/com/ingaia/apistarter/util/Util.java
+++
@@ -1,9 +0,0 @@
-package com.ingaia.apistarter.util;
-
-public class Util {
-	
-	public static int getRandomNumber(int min, int max) {
-	    return (int) ((Math.random() * (max - min)) + min);
-	}
-
-}
